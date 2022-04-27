66297fe6efdaf27a266af6a6578a135b1c9b4ff1
metacoder87 <gary.miller@gmx.com>
2022-04-26 23:39:53 -0400

    Adds cat new and create

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -6,11 +6,18 @@ class CatsController < ApplicationController
     end
 
     def create
-
+        @cat = Cat.new(cat_params)
+        if @cat.save
+            render cat_url(@cat)
+        else 
+            flash.now[:errors] = @cat.errors.full_messages
+            render :new
+        end
     end
 
     def new
-
+        @cat = Cat.new
+        render :new
     end
 
     def edit
