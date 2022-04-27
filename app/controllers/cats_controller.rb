35385c761a1937ae136182e1b7a2a77f4abd9eae
metacoder87 <gary.miller@gmx.com>
2022-04-27 01:20:31 -0400

    Moves around some methods

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -5,14 +5,9 @@ class CatsController < ApplicationController
         render :index
     end
 
-    def create
-        @cat = Cat.new(cat_params)
-        if @cat.save
-            render cat_url(@cat)
-        else 
-            flash.now[:errors] = @cat.errors.full_messages
-            render :new
-        end
+    def show
+        @cat = Cat.find(params[:id])
+        render :show
     end
 
     def new
@@ -20,14 +15,19 @@ class CatsController < ApplicationController
         render :new
     end
 
+    def create
+        @cat = Cat.new(cat_params)
+        if @cat.save
+            redirect_to cat_url(@cat)
+        else 
+            flash.now[:errors] = @cat.errors.full_messages
+            render :new
+        end
+    end
+
     def edit
         @cat = Cat.find(params[:id])
         render :edit
-    end
-
-    def show
-        @cat = Cat.find(params[:id])
-        render :show
     end
 
     def update
@@ -43,6 +43,6 @@ class CatsController < ApplicationController
 private
 
     def cat_params
-        params.require(:cat).permit(:name, :birth_date, :color, :sex, :description)
+        params.require(:cat).permit(:name, :birth_date, :age, :color, :sex, :description)
     end
 end
