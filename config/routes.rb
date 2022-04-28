17a4b9723aec86945df24c624428f49f2d69172f
metacoder87 <gary.miller@gmx.com>
2022-04-27 21:28:21 -0400

    Adds resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cats, except: :destroy
+
+  resources :cats_rental_requests, only: [:create, :new] do
+    member do
+      post :approve
+      post :deny
+    end
+  end
   
   root to: redirect('/cats')
 end
