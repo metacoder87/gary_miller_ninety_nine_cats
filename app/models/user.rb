d691aaea3b6255651bb2904730542f707098101e
metacoder87 <gary.miller@gmx.com>
2022-05-04 00:23:43 -0400

    Adds users model with validations and ::find by

---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+class User < ApplicationRecord
+    validates :username, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: { message: 'Password can\'t be blank' }
+    validates :password, length: { minimum: 6, allow_nil: true }
+    after_initialize :ensure_session_token
+
+    attr_reader :password
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        return user if user && BCrypt::Password.new(user.password_digest).is_password?(password)
+        nil
+    end
+
+    def reset_session_token!
+        SecureRandom
+    end
+
+    def password=(password)
+
+    end
+
+    def is_password?(password)
+
+    end
+end
