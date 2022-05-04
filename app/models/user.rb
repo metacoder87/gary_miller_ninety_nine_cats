8d3b895c52ad709e9960414464dddf88bb73c30a
metacoder87 <gary.miller@gmx.com>
2022-05-04 17:35:07 -0400

    Privatizes a method

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,16 +8,12 @@ class User < ApplicationRecord
 
     def self.find_by_credentials(username, password)
         user = User.find_by(username: username)
-        return user if user && is_password?(password)
+        return user if user && user.is_password?(password)
         nil
     end
 
     def self.generate_session_token
         SecureRandom::urlsafe_base64(16)
-    end
-
-    def ensure_session_token
-        self.session_token ||= User.generate_session_token
     end
 
     def reset_session_token!
@@ -33,5 +29,11 @@ class User < ApplicationRecord
 
     def is_password?(password)
         BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+private
+
+    def ensure_session_token
+        self.session_token ||= User.generate_session_token
     end
 end
