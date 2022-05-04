4cb92c17bd995697d09c2f08c9f86239b400b4e3
metacoder87 <gary.miller@gmx.com>
2022-05-04 18:01:39 -0400

    Adds users create control

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,17 @@ class UsersController < ApplicationController
         @user = User.new
         render :new
     end
-    
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            login_user!(@user)
+            redirect_to cats_url
+        else
+            flash.now[:errors] = @user.errors.full_messages
+            render :new
+        end
+    end
 
 private
 
