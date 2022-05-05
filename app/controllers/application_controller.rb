64b9e89c983c88e0715f30399bf42e3cf4f7d287
metacoder87 <gary.miller@gmx.com>
2022-05-04 21:22:15 -0400

    Adds require user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,16 @@ private
         !current_user.nil?
     end
 
+    def login_user!(user)
+        currect_user.reset_session_token!
+        session[:session_token] = nil
+    end
+
     def require_no_user!
         redirect_to cats_url if current_user
+    end
+
+    def require_user!
+        redirect_to new_session_url if current_user.nil?
     end
 end
