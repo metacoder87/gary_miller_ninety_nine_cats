42103ca937a5cb96689e03f10b2a43c76e3e3643
metacoder87 <gary.miller@gmx.com>
2022-05-04 21:03:37 -0400

    Adds Sessions create controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,18 @@ class SessionsController < ApplicationController
     end
 
     def create
-        
+        user = User.find_by_credentials(
+            params[:user][:username],
+            params[:user][:password]
+        )
+
+        if user.nil?
+            flash.now[:errors] = ["Incorrect username and/or password."]
+            render :new
+        else
+            login_user!(user)
+            redirect_to cats_url
+        end
     end
 
     def destroy
