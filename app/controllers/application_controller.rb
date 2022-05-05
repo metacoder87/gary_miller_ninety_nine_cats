ab59e9515bb6f1cb8ef4c3e8d3ed73e4a14e729d
metacoder87 <gary.miller@gmx.com>
2022-05-04 21:18:03 -0400

    Adds current user and helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    helper_method :current_user
+    helper_method :logged_in?
+
+private
+
+    def current_user
+        return nil unless session[:session_token]
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
 end
