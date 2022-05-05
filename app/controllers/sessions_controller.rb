e716d7acbe504bc6361e2116142a61e7168eaf3d
metacoder87 <gary.miller@gmx.com>
2022-05-04 20:55:52 -0400

    Adds Sessions new controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,15 @@
 class SessionsController < ApplicationController
 
     def new
-
+        render :new
     end
 
     def create
-
+        
     end
 
     def destroy
-
+        
     end
 
 end
