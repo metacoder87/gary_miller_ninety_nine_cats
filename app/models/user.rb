541c9778c39b5d4366ab887c1c5ffdcc330f05cf
metacoder87 <gary.miller@gmx.com>
2022-05-10 02:07:22 -0400

    Method rewrite

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,8 @@ class User < ApplicationRecord
 
     def self.find_by_credentials(username, password)
         user = User.find_by(username: username)
-        return user if user && user.is_password?(password)
-        nil
+        return nil if user.nil?
+        user.is_password?(password) ? user : nil
     end
 
     def self.generate_session_token
