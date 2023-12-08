bd26313dab3fcd41a6c268006cbaa474847c5323
metacoder87 <gary.miller@gmx.com>
2023-12-08 18:16:53 -0500

    Updates to render build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,8 +2,8 @@
 # exit on error
 set -o errexit
 
-bundle _2.2.34_ install
-RAILS_ENV=production bundle exec rake assets:precompile
-RAILS_ENV=production bundle exec rake db:migrate --trace
-RAILS_ENV=production bundle exec rake db:seed
+bundle install
+bundle exec rake assets:precompile
+bundle exec rake db:migrate
+bundle exec rake db:seed
 bundle exec rake assets:clean
