a41bddaa6314672bc04363a7819f6ca8648a0459
metacoder87 <gary.miller@gmx.com>
2025-08-27 17:39:47 -0400

    Fixes bugs associated with the Ruby update

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -33,7 +33,7 @@ class CatsController < ApplicationController
 
     def update
         @cat = current_user.cats.find(params[:id])
-        if @cat.update_attributes(cat_params)
+        if @cat.update(cat_params)
             redirect_to cat_url(@cat)
         else 
             flash.now[:errors] = @cat.errors.full_messages
